b1e1dbca552bf4a4950817903755194b4c0a82f8
arnavmittal2009 <87121694+arnavmittal2009@users.noreply.github.com>
2021-10-09 12:44:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,26 @@ function take_snapshot()
 
 console.log('ml5 version:',ml5.version);
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/kMiifm07r/model.json',modelLoaded);
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error) {
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
